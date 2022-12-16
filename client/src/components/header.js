7a906bd426b8088fdcb09fe62ee2d5bb0bf24682
dvdjms <dvdjms@hotmail.com>
2022-12-16 17:08:13 +0000

    worked navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,76 @@
 import React from 'react'
-
+import styled from 'styled-components';
 
 const Header = () => {
 
-
     return (
-        <h1>This is the header (navbar)</h1>
-      )
     
+        <HeaderContainer>
+            <HeaderOne>
+                <H1><A href="/">Learn Scotland</A></H1>
+            </HeaderOne>
+            <HeaderTwo>
+                <UnorderedList>
+                    <List><A href="/">Home</A></List>
+                    <List><A href="/">Quizzes</A></List>
+                    <List><A href="/">More Quizzes</A></List>
+                    <List><A href="/">Facts</A></List>
+                    <List><A href="/">Forum</A></List>
+                </UnorderedList>
+            </HeaderTwo>
+        </HeaderContainer>
+    )
 }
+
+const HeaderContainer = styled.header`
+    height: 6.3vw;
+    background-color: darkblue;
+`;
+
+const HeaderOne = styled.div`
+    float: left;
+    height: 100%;
+    width: 25%;
+`;
+
+const HeaderTwo = styled.div`
+    float: left;
+    margin-bottom: 5vw;
+    width: 70%;
+`;
+
+const H1 = styled.h1`
+    color: white;
+    font-family: Impact, 'Arial Narrow Bold', sans-serif;
+    font-size: 3vw;
+    margin-top: 0;
+    padding-top: 5%;
+    padding-left: 10%;
+`;
+
+const UnorderedList = styled.ul`
+    padding-top: 1.9%;
+    list-style: none;
+`;
+
+const List = styled.li`
+    height: 100%;
+    font-size: 1.4vw;
+    float: left;
+    margin-right: 2vw;
+`;
+
+const A = styled.a`
+    color: white;
+    text-decoration: none;
+    font-weight: 600;
+    &:hover {
+        color: yellow;
+    }
+    &:active {
+        color: green;
+    }
+`;
+
 
 export default Header;
