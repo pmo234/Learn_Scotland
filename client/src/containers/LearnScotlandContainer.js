e42d788998d5687fc3589f0759929f62b2690929
dvdjms <dvdjms@hotmail.com>
2022-12-16 14:03:45 +0000

    setting up files

---
+++ client/src/containers/LearnScotlandContainer.js
@@ -0,0 +1,16 @@
+import Header from '../components/header';
+import LandingPage from '../components/landingPage';
+
+
+
+const LearnScotlandContainer = () => {
+
+    return (
+        <>
+        <Header />
+        <LandingPage />
+        </>
+    )
+}
+
+export default LearnScotlandContainer;
