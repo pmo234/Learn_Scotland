1e6e60dd4ee09d3cc23a2e644988f65e3e9c55a7
dvdjms <dvdjms@hotmail.com>
2022-12-16 12:21:44 +0000

    database functions working

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+
+// const cors = require("cors");
+
+// app.use(cors())
+
+
+app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
