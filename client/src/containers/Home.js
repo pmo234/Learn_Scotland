f2ffafc8071ce4ca298ff21e0b55263840608711
user <user@users-MacBook-Pro.local>
2022-12-19 15:18:36 +0000

    homepage and leaderboard

---
+++ client/src/containers/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from '../components/Header'
+import Leaderboard from '../components/Leaderboard'
+import { useEffect,useState } from 'react';
+const baseURL = "http://localhost:9000/api/users/";
+
+export default function Home() {
+
+  const [users, setUsers] = useState([]);
+    useEffect(()=>{
+        getUsers()
+    },[]
+    )
+
+    const getUsers = () => {
+        return fetch(baseURL)
+          .then((res) => res.json())
+          .then((results) => setUsers(results));
+      };
+
+      
+  return (
+    <div>
+        <Header/>
+        <Leaderboard users = {users}/>
+        
+        
+    </div>
+  )
+}
--- client/src/containers/LearnScotlandContainer.js
+++ client/src/containers/LearnScotlandContainer.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuizSingleContainer from "./quizSingleContainer";
 import LandingPage from "../components/LandingPage";
 import Trial from "../components/Trial";
+import Forum from "./Forum";
+import Home from "./Home";
 
 const LearnScotlandContainer = () => {
   return (
@@ -11,6 +13,8 @@ const LearnScotlandContainer = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/quizSingle" element={<QuizSingleContainer/>} />
           <Route path="/trial" element={<Trial />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </>
