55ce6251edcb8f67eb542194f963b6bc7cab73b9
1919dev <sfbsturrock@gmail.com>
2022-12-20 10:56:47 +0000

    start map quiz

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -21,6 +21,7 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <Leaderboard users={users}/>
     </div>
   );
 }
--- server/db/mapseeds.js
+++ server/db/mapseeds.js
@@ -3,4 +3,25 @@
   "position": {"lat": 56.7969, "lng": -5.0036},
   "question": "Scotland, and the UK's tallest mountain is Ben Nevis. Can you click on it's position?"
 }
+{
+  "img": "",
+  "position": {"lat": 56.1388, "lng": -3.9179},
+  "question": "This is the Wallace monument, a landmark built to commemorate William Wallace's victory over the English army. It overlooks the battlefield, but do you know where it is?"
+  "info": "The victory was at the Battle of Stirling Bridge, so unsurprisingly the monument "
+}
+{
+  "img": "",
+  "position": {"lat": 56.7969, "lng": -5.0036},
+  "question": "Scotland, and the UK's tallest mountain is Ben Nevis. Can you click on it's position?"
+}
+{
+  "img": "",
+  "position": {"lat": 56.7969, "lng": -5.0036},
+  "question": "Scotland, and the UK's tallest mountain is Ben Nevis. Can you click on it's position?"
+}
+{
+  "img": "",
+  "position": {"lat": 56.7969, "lng": -5.0036},
+  "question": "Scotland, and the UK's tallest mountain is Ben Nevis. Can you click on it's position?"
+}
 
